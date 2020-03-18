4f8fc741183e743456779cf634020dce251db636
Julio Guerra <julio@sqreen.com>
2020-03-18 11:02:49 +0100

    signal/client: bare http client of the api
    Bare Go implementation of the singal ingestion HTTP API with:
    - newRequest: request creator with json body.
    - do: perform the given request, check the response for errors, and get the json
      body

---
+++ signal/client/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2016 - 2020 Sqreen. All Rights Reserved.
+// Please refer to our terms for more information:
+// https://www.sqreen.io/terms.html
+
+// This package is the bare HTTP client for security signals.
+package client
